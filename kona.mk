0e4995679684b52e5d28bfbdb81867e4b44a5daf
Zinadin Zidan <zidan.roking@gmail.com>
2021-09-09 00:22:52 +0600

    sm8250-common: Guard qti thermal 2.0 service hals
    * Its crashes on renoir
    
     * Crash:
        - E android.hardware.thermal@2.0-service.qti: ThermalHAL not initialized properly.
        - E ThermalHalWrapper: Couldn't get temperatures because of HAL error: ThermalHAL not initialized properly.
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I56d844402e8ec6d8e0dfd145495cc19619ae7b75
    
    - Also crashes on alioth

--- kona.mk
+++ kona.mk
@@ -447,8 +447,10 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Thermal
+ifeq ($(TARGET_USE_QTI_THERMAL_SERVICE),true)
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
+endif
 
 # Touchfeature
 PRODUCT_PACKAGES += \
