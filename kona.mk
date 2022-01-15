178258b6b2c2dd15ceadd2b6b0c85d499b874566
klozz <klozz@thexperienceproject.org>
2022-01-16 03:24:16 +0530

    sm8250-common: fix drm issues
    Signed-off-by: klozz <klozz@thexperienceproject.org>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- kona.mk
+++ kona.mk
@@ -247,6 +247,9 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.4.vendor \
     android.hardware.drm@1.4-service.clearkey
 
 PRODUCT_PACKAGES += \
