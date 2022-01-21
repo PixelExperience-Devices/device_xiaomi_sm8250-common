408fadffafad9e37c7e5fd815d47ec294867f8c0
chandra <chandra.chaganti@hotmail.com>
2022-01-21 10:55:58 +0530

    sm8250-common: drop lineage trust

--- kona.mk
+++ kona.mk
@@ -467,10 +467,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.2-service-qti
