5245c6bd206023f97cb7668eab06cb8a5b907f82
Dobsgw <george.w.dobson@gmail.com>
2022-01-29 08:06:03 +0300

    sm8250-common: Partial revert of broken ass bringup
    Change-Id: Ie9625358de1bf29dbc285907ab0eb3bff3300c03

--- kona.mk
+++ kona.mk
@@ -457,8 +457,8 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-#PRODUCT_BOOT_JARS += \
-#    telephony-ext
+PRODUCT_BOOT_JARS += \
+    telephony-ext
 
 # Thermal
 PRODUCT_PACKAGES += \
