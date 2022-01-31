dcabf854df578d8000b250c024d15eb6f3035568
chandra <chandra.chaganti@hotmail.com>
2022-01-31 17:26:45 +0530

    Revert "sm8250-common: fix drm issues"
    This reverts commit 178258b6b2c2dd15ceadd2b6b0c85d499b874566.

--- kona.mk
+++ kona.mk
@@ -248,9 +248,6 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4.vendor \
     android.hardware.drm@1.4-service.clearkey
 
 PRODUCT_PACKAGES += \
