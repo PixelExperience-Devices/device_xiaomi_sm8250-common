a15abfee5e4d90bffef0809055be749995eab0f8
LiMee233 <LiMee233@outlook.com>
2022-06-05 14:54:34 +0800

    Revert "sm8250-common: Add hardware/qcom-caf/wlan to namespaces and wpa_supplicant_8_lib to cfi path" * Useless.
    This reverts commit 692d525dd79d8fd99f08a7af998a1c7bfad64a4a

--- kona.mk
+++ kona.mk
@@ -421,8 +421,7 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi \
-    hardware/qcom-caf/wlan \
+    hardware/xiaomi
 
 # Telephony
 PRODUCT_PACKAGES += \
@@ -492,10 +491,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
-
-# Wlan
-PRODUCT_CFI_INCLUDE_PATHS += \
-    hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sm8250-common/sm8250-common-vendor.mk)
