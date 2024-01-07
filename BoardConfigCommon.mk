18f6c223a51448c1e855546f82ccdef0dcfb539a
Arne Coucheron <arco68@gmail.com>
2024-01-07 23:44:46 +0000

    sm8250-common: Set TARGET_DISABLE_POSTRENDER_CLEANUP := true To fix the bad UI lag we are experiencing.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -75,6 +75,9 @@ ifeq ($(TARGET_HAS_UDFPS),true)
 TARGET_USES_FOD_ZPOS := true
 endif
 
+# Disable Postrender Cleanup
+TARGET_DISABLE_POSTRENDER_CLEANUP := true
+
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
